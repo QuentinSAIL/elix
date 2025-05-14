f9201e518a274739ae2105a8e3772d249864edd6
Quentin SAILLARD <saillardq@gmail.com>
2025-05-15 00:59:48 +0200

    WIP: transaction dashboard chart displayed

--- resources/views/livewire/money/dashboard.blade.php
+++ resources/views/livewire/money/dashboard.blade.php
@@ -1,11 +1,9 @@
-    <div class="flex flex-row gap-4">
+    <div class="flex flex-row flex-wrap gap-4">
         @if ($moneyDashboardPanels)
             @foreach ($moneyDashboardPanels as $moneyDashboardPanel)
-                <div class="p-8 h-full bg-custom-accent cursor-pointer">
-                    <div>
-                        <livewire:money.dashboard-panel :moneyDashboardPanel="$moneyDashboardPanel" />
-                    </div>
+                <div class="p-8 w-1/3 h-full bg-custom-accent cursor-pointer">
+                    <livewire:money.dashboard-panel :moneyDashboardPanel="$moneyDashboardPanel" />
                 </div>
             @endforeach
-        @endif
+            <div class="p-8 w-1/3 h-full bg-custom-accent cursor-pointer"><span>Ajouter un panneau <flux:icon.plus /></span></div>    @endif
     </div>
