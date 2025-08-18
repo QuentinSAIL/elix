e7f5117204a15b52658d7dbf0be1a39e7a16c073
Quentin SAILLARD <saillardq@gmail.com>
2025-08-18 17:42:08 +0200

    feat: feat: implement global rate limiting for web middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -35,6 +38,15 @@ class AppServiceProvider extends ServiceProvider
             $limit = $parts[1] ?? 100;
 
             return "<?php echo e(Str::limit({$string}, {$limit})); ?>";
+        });
+
+        // Rate limiters
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(200)->by(optional($request->user())->id ?: $request->ip());
+        });
+
+        RateLimiter::for('money', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
